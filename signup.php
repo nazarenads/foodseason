c23ff78bdbdea908b2210c675f3fd1a0ef4994fd
nazarenads <nazarenads@gmail.com>
2019-05-11 21:21:20 -0300

    organizo archivos php

--- signup.php
+++ signup.php
@@ -1,3 +1,93 @@
+<?php
+//Defino variables para cada error posible
+
+$errorUsername = "";
+$errorEmail = "";
+$errorPassword = "";
+$errorPicture = "";
+$hayErrores = false;
+
+//Si llega algo por POST
+
+if($_POST){
+
+  //Tomo lo recibido y lo guardo sin espacios
+  $username = trim($_POST["username"]);
+  $email = trim($_POST["email"]);
+  $password = trim($_POST["password"]);
+  $passwordConfirm = trim($_POST["passwordConfirm"]);
+  $profilePicture = $_FILES["profilePicture"];
+
+  //Valido cada dato
+  if($username == ""){
+    $errorUsername = "Completá tu nombre de usuario!";
+    $hayErrores = true;
+  }
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    $errorEmail = "El email que ingresaste no es válido";
+    $hayErrores = true;
+  }
+  if($password == ""){
+    $errorPassword = "Completá tu contraseña!";
+    $hayErrores = true;
+  }
+  else if (strlen($password)<4)
+    {
+      $errorPassword = "Tu contraseña debe tener al menos 4 caracteres!";
+      $hayErrores = true;
+    }else if($password != $passwordConfirm){
+      $errorPassword = "Las contraseñas que ingresaste no coinciden!";
+      $hayErrores = true;
+    }
+
+  //Si recibo una foto de perfil
+
+  if(isset($_FILES["profilePicture"])){
+      //si el dato de error es OK...
+    if($_FILES["profilePicture"]["error"] === UPLOAD_ERR_OK){
+      //guardo el nombre del archivo
+      $pictureName = $_FILES["profilePicture"]["name"];
+      //guardo el nombre temporal del archivo
+      $origen = $_FILES["profilePicture"]["tmp_name"];
+      //uso la informacion del path que es la url, para tomar y guardar la extension
+      $ext = pathinfo($pictureName,PATHINFO_EXTENSION);
+
+      $destino = "";
+      $destino = $destino."archivos/";
+      //genero la ruta donde guardo el archivo
+      $destino = $destino.$username."fotodeperfil.".$ext;
+
+      //guardo el archivo con esta funcion
+      move_uploaded_file($origen,$destino);
+      $errorPicture = "Subiste tu foto con éxito!";
+      }
+    }else{
+        $errorPicture = "Ups! Hubo un error al subir tu foto";
+        $hayErrores = true;
+    }
+
+// Si no hay ningún error guardo los datos validados en un array asociativo
+    if(!$hayErrores){
+      $userArray = [
+      "username"=> $username,
+      "email"=> $email,
+      "profilePicture" => $username."profilePicture.".$ext,
+      "password" => password_hash($password, PASSWORD_DEFAULT)];
+      $userJson = json_encode($userArray);
+      file_put_contents('usuarios.json',$userJson);
+      header('Location:http://google.com');
+    }
+
+
+
+
+}
+
+
+
+
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -21,49 +111,35 @@
            <div class="formulario">
 
 
-    		  <form action="signup.php" method="post" class="form-signup">
+    		  <form action="signup.php" method="post" class="form-signup" enctype="multipart/form-data">
+
     		   <h3 class="form-signup-heading">¡Registrate!</h3>
     		   <div class="form-group">
-    		    <input name="email" type="text" class="form-control" placeholder="Email">
+    		    <input name="email" type="text" class="form-control" placeholder="Email" value="">
+            <?= $errorEmail ?>
     		   </div>
     		   <div class="form-group">
-    		    <input name="username" type="text" class="form-control" placeholder="Usuario">
+    		    <input name="username" type="text" class="form-control" placeholder="Usuario" value="">
+            <?= $errorUsername ?>
+           </div>
+            <div class="form-group">
+    		    <input name="profilePicture" type="file" class="form-control" value="">
+            <?= $errorPicture ?>
     		   </div>
     		   <div class="form-group">
-    		    <input type="password" class="form-control" name="password" placeholder="Contraseña">
+    		    <input type="password" class="form-control" name="password" placeholder="Contraseña" value="">
+            <?= $errorPassword ?>
+           </div>
+            <div class="form-group">
+    		    <input type="password" class="form-control" name="passwordConfirm" placeholder="Confirmar contraseña" value= "">
     		   </div>
-    		   <button class="btn btn-warning" style="width:100%" type="submit" name="subm">Enviar</button>
+    		   <button class="btn btn-warning" style="width:100%" type="submit" name="submit">Enviar</button>
     		   <br/><br>
     		   <a class="btn btn-light" href="signin.php" role="button">¿Ya tenés una cuenta?</a>
     		   <a class="btn btn-light" href="#" role="button">¿Te olvidaste tu contraseña?</a>
-    		  </form>
+
+          </form>
 
            </div>
          </div>
   </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
